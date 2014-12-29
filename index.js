57af793ae678aec47c4b6305848c1e6d710bf7e9
Tyler Sanderson <tylerbtbam@gmail.com>
2014-12-29 23:07:03 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+var express = require('express')
+var cv = require('opencv')
+var app = express();
+var viewPath = 'views/'
+
+app.set('port', (process.env.PORT || 8080))
+app.set('view engine', 'jade')
+app.set('views', (__dirname + '/views'))
+app.use(express.static(__dirname + '/public'))
+
+app.get('/', function(request, response) {
+    response.render('index')
+})
+
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'))
+})
