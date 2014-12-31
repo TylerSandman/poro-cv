a80e7e9a7fef1c73f35338ee6162e77308acfcde
Tyler Sanderson <tylerbtbam@gmail.com>
2014-12-30 21:33:14 -0500

    Added support for drag and drop and click to upload files

--- public/javascripts/filehandling.js
+++ public/javascripts/filehandling.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
 var uploader = $("#uploader");
 var fileInput = $("#input1");
+var img = $("#uploaded-img");
 
 function isValid(file){
 	//Less than 2MB and valid formats
@@ -8,11 +9,16 @@ function isValid(file){
 			(file.type === "image/jpg" || file.type === "image/png" || file.type === "image/bmp" || file.type === "image/jpeg"))
 }
 
-//Click to upload
-uploader.on("click", function(){
-	fileInput.click();
-})
+function loadImg(file){
 
+	var reader = new FileReader();  
+	console.log(file)
+	reader.onload = function (e) {
+		console.log(e)
+		img.attr('src', e.target.result);
+	}		
+	reader.readAsDataURL(file);
+}
 
 //Drag and drop events
 function UploaderDragHover(e){	
@@ -23,11 +29,28 @@ function UploaderDragHover(e){
 function UploaderDrop(e){
 	UploaderDragHover(e);
 	var files = e.originalEvent.dataTransfer.files;
-	if (isValid(files[0])){
+	var file = files[0];
+	if (isValid(file)){
+		loadImg(file)
 	}
 }
+
+//Upload from input rather than drag and drop
+function InputUpload(e){
+	var files = e.target.files;
+	var file = files[0];
+	if (isValid(file)){
+		loadImg(file)
+	}
+}
+
+//Click to upload
+uploader.on("click", function(){
+	fileInput.click();
+})
 
 uploader.on("dragover", UploaderDragHover);
 uploader.on("dragleave", UploaderDragHover);
 uploader.on("drop", UploaderDrop);
+fileInput.on("change", InputUpload);
 })
