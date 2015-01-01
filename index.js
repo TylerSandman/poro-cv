2c5dbca9e38aeec16b11a000863f153000a7b64f
Tyler Sanderson <tylerbtbam@gmail.com>
2015-01-01 17:33:32 +0000

    Tab cleanup

--- index.js
+++ index.js
@@ -14,19 +14,19 @@ var cascadeFile = __dirname + '/public/banana_classifier.xml';
 
 function detectPoros(request, response, path){
 
-	exec('python ' + __dirname + '/public/recognizer.py ' + path,
-	function (error, stdout, stderr) {
-		if (error !== null) {
-			console.log('exec error: ' + error);
-		}
+    exec('python ' + __dirname + '/public/recognizer.py ' + path,
+    function (error, stdout, stderr) {
+        if (error !== null) {
+            console.log('exec error: ' + error);
+        }
         //Remove all uploads
         var split = path.split("/");
         if (split[split.length-2] === "uploads"){
             fs.unlink(path);
         }
-	    response.writeHead(200, { 'Connection': 'close' });
-	    response.end(stdout);
-	});
+        response.writeHead(200, { 'Connection': 'close' });
+        response.end(stdout);
+    });
 }
 
 app.get('/', function(request, response) {
@@ -34,29 +34,29 @@ app.get('/', function(request, response) {
 });
 
 app.post('/process', function(request, response) {
-	request.pipe(request.busboy);
-	
-	//Upload request
-	request.busboy.on('file', function(fieldname, file, filename){
-		console.log("Uploading: " + filename);
-		var path = __dirname + '/public/uploads/' + filename;
-		fstream = fs.createWriteStream(path);
-		file.pipe(fstream);
-		fstream.on('close', function(){
-			console.log("Processing: " + filename);
-			detectPoros(request, response, path);
-		});
-	});
-	
-	//Local processing request
-	request.busboy.on('field', function(fieldname, val, fieldnameTruncated, valTruncated) {
-	
-		if (fieldname !== 'text') return;
-		console.log("Processing: " + val);
-		detectPoros(request, response, val);
-	});
+    request.pipe(request.busboy);
+    
+    //Upload request
+    request.busboy.on('file', function(fieldname, file, filename){
+        console.log("Uploading: " + filename);
+        var path = __dirname + '/public/uploads/' + filename;
+        fstream = fs.createWriteStream(path);
+        file.pipe(fstream);
+        fstream.on('close', function(){
+            console.log("Processing: " + filename);
+            detectPoros(request, response, path);
+        });
+    });
+    
+    //Local processing request
+    request.busboy.on('field', function(fieldname, val, fieldnameTruncated, valTruncated) {
+    
+        if (fieldname !== 'text') return;
+        console.log("Processing: " + val);
+        detectPoros(request, response, val);
+    });
 });
 
 app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'));
+    console.log("Node app is running at localhost:" + app.get('port'));
 });
--- public/javascripts/filehandling.js
+++ public/javascripts/filehandling.js
@@ -5,14 +5,14 @@ var img = $("#uploaded-img");
 var resultText = $("#result-text");
 
 function updateResultText(detected){
-	if (detected){
-	    resultText.text("I see a poro! Look at the cute little guy.");
-	}
-	else{
-            resultText.text("I don't see any poros in this image...");
+    if (detected){
+        resultText.text("I see a poro! Look at the cute little guy.");
     }
-
+    else{
+        resultText.text("I don't see any poros in this image...");
+    }
 }
+
 function isValid(file){
 
     //Less than 2MB 
@@ -25,111 +25,111 @@ function isValid(file){
          (file.type === "image/png")||
          (file.type === "image/bmp"));
 
-	return (validSize && validFormat);
+    return (validSize && validFormat);
 }
 
 function loadPreview(file){
 
-	var reader = new FileReader(); 
-	reader.onload = function (e) {
-		img.attr('src', e.target.result);
-	}
-	reader.readAsDataURL(file);
+    var reader = new FileReader(); 
+    reader.onload = function (e) {
+        img.attr('src', e.target.result);
+    }
+    reader.readAsDataURL(file);
 }
 
 function uploadImage(file){
 
-	var reader = new FileReader();  
-	var fd = new FormData();
-	fd.append('file', file);
-	uploader.addClass('spinner');
-	$.ajax({
-		url: '/process',
-		type: "POST",
-		contentType: false,
-		success: function(data){
-			console.log("Success!")
-			updateResultText(data.detected)
-			uploader.removeClass('spinner')
-		},
-		error: function(jqXHR, textStatus, errorThrown){
-			console.log("Error with processing request!")
-			console.log(textStatus)
-			console.log(errorThrown)
-			uploader.removeClass('spinner')
-		},
-		data: fd,
-		dataType: 'json',
-		processData: false
-	});
+    var reader = new FileReader();  
+    var fd = new FormData();
+    fd.append('file', file);
+    uploader.addClass('spinner');
+    $.ajax({
+        url: '/process',
+        type: "POST",
+        contentType: false,
+        success: function(data){
+            console.log("Success!")
+            updateResultText(data.detected)
+            uploader.removeClass('spinner')
+        },
+        error: function(jqXHR, textStatus, errorThrown){
+            console.log("Error with processing request!")
+            console.log(textStatus)
+            console.log(errorThrown)
+            uploader.removeClass('spinner')
+        },
+        data: fd,
+        dataType: 'json',
+        processData: false
+    });
 }
 
 function processImage(src){
-	//Send post request with local file source to work with busboy
-	var fd = new FormData();
-	fd.append('text', src);
-	uploader.addClass('spinner');
-	$.ajax({
-		url: '/process',
-		type: "POST",
-		contentType: false,
-		success: function(data){
-			console.log("Success!")
-			updateResultText(data.detected)
-			uploader.removeClass('spinner')
-		},
-		error: function(jqXHR, textStatus, errorThrown){
-			console.log("Error with processing request!")
-			console.log(textStatus)
-			console.log(errorThrown)
-			uploader.removeClass('spinner')
-		},
-		data: fd,
-		dataType: 'json',
-		processData: false
-	});
+    //Send post request with local file source to work with busboy
+    var fd = new FormData();
+    fd.append('text', src);
+    uploader.addClass('spinner');
+    $.ajax({
+        url: '/process',
+        type: "POST",
+        contentType: false,
+        success: function(data){
+            console.log("Success!")
+            updateResultText(data.detected)
+            uploader.removeClass('spinner')
+        },
+        error: function(jqXHR, textStatus, errorThrown){
+            console.log("Error with processing request!")
+            console.log(textStatus)
+            console.log(errorThrown)
+            uploader.removeClass('spinner')
+        },
+        data: fd,
+        dataType: 'json',
+        processData: false
+    });
 }
 
 //Drag and drop events
-function UploaderDragHover(e){	
-	e.preventDefault();
-	e.target.className = (e.type == "dragover" ? "hover" : "");
+function UploaderDragHover(e){  
+    e.preventDefault();
+    e.target.className = (e.type == "dragover" ? "hover" : "");
 }
 
 function UploaderDrop(e){
-	UploaderDragHover(e);
-	var files = e.originalEvent.dataTransfer.files;	
-	var file = files[0];
-	
-	//Not uploaded
-	if (!file){
-		var src = e.originalEvent.dataTransfer.getData("src", '');
-		var name = e.originalEvent.dataTransfer.getData("name");
-		if (src !== ''){
-			img.attr('src', src);
-			processImage("/home/ubuntu/poro-cv/public/images/examples/" + name);
-		}
-	}
-	
-	else if (isValid(file)){
-		loadPreview(file);
-		uploadImage(file);
-	}
+    UploaderDragHover(e);
+    var files = e.originalEvent.dataTransfer.files; 
+    var file = files[0];
+    
+    //Not uploaded
+    if (!file){
+        var src = e.originalEvent.dataTransfer.getData("src", '');
+        var name = e.originalEvent.dataTransfer.getData("name");
+        if (src !== ''){
+            img.attr('src', src);
+            processImage("/home/ubuntu/poro-cv/public/images/examples/" + name);
+        }
+    }
+    
+    else if (isValid(file)){
+        loadPreview(file);
+        uploadImage(file);
+    }
 }
 
 //Upload from input rather than drag and drop
 function InputUpload(e){
-	var files = e.target.files;
-	var file = files[0];
-	if (isValid(file)){
-		loadPreview(file);
-		uploadImage(file);
-	}
+    var files = e.target.files;
+    var file = files[0];
+    if (isValid(file)){
+        loadPreview(file);
+        uploadImage(file);
+    }
 }
 
 //Click to upload
 uploader.on("click", function(){
-	fileInput.click();
+    fileInput.click();
 })
 
 uploader.on("dragover", UploaderDragHover);
@@ -137,10 +137,10 @@ uploader.on("dragleave", UploaderDragHover);
 uploader.on("drop", UploaderDrop);
 fileInput.on("change", InputUpload);
 $("img").on("dragstart", function(e){
-	var src = e.target.src;
-	var name = src.split("/");
-	name = name[name.length-1];
-	e.originalEvent.dataTransfer.setData("src", src);
-	e.originalEvent.dataTransfer.setData("name", name);
+    var src = e.target.src;
+    var name = src.split("/");
+    name = name[name.length-1];
+    e.originalEvent.dataTransfer.setData("src", src);
+    e.originalEvent.dataTransfer.setData("name", name);
 });
 });
